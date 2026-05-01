a03e50cb0f7cdd9bbcadadc470d4f96d573d629c
kh7di <yaayekd@gmail.com>
2026-05-01 12:57:11 -0400

    update gallery images, add js to populate gallery

--- garden/scripts/gallery.js
+++ garden/scripts/gallery.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('#image-carousel').mount();
 });
+
+const flowerImgs = [];
+const galleryContainer = document.querySelector(".splide__list");
+const folderPath = "./images/flower_imgs/flower ";
+for(let i = 1; i <= 16; i++){
+    flowerImgs.push(`${folderPath}(${i}).png`);
+}
+
+function addPlantsToPage(){
+    flowerImgs.forEach(img => {
+        const flowerImg = document.createElement("li");
+        flowerImg.classList.add("splide__slide");
+        flowerImg.innerHTML = `<img src="${img}" alt="flower">`;
+        galleryContainer.appendChild(flowerImg);
+    });
+}
+
+addPlantsToPage();
